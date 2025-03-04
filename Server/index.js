5015936c80e57d3651ef292f4f467af21c2e6fbd
VishalSoni007 <vsoni0882@gmail.com>
2025-03-04 21:33:01 +0530

    file structure

--- Server/index.js
+++ Server/index.js
@@ -5,7 +5,11 @@ const app = express();
 
 app.use(cors(
     {
-        origin: 'http://localhost:5173', // Change this to your frontend's URL
+        origin: 'http://localhost:5173', 
         credentials: true,
     }
 ));
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
