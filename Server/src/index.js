f693d3e6b825d969cbce1ad0111499f89fbce5f3
VishalSoni007 <vsoni0882@gmail.com>
2025-03-05 21:41:34 +0530

    Folder Structure

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,15 +1,16 @@
 import express from "express";
 import cors from "cors";
+import authRoute from "./Routes/auth.route.js";
 
 const app = express();
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true
-
-
   })
 );
+
+app.use('/api/auth', authRoute)
 const PORT = 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
