551ef4fcfc18696928ba08743e724cc9892c372e
VishalSoni007 <vsoni0882@gmail.com>
2025-03-07 21:42:14 +0530

    Frontend Progress

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,25 @@
-import './App.css'
+import { Route, Routes } from "react-router";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import SettingPage from "./pages/SettingPage";
+import ProfilePage from "./pages/ProfilePage";
+import Navbar from "./components/Navbar.component";
 
 function App() {
-
   return (
     <div>
-      
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/settings" element={<SettingPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
