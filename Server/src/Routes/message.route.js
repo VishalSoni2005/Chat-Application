a5454f2aa7b418a665a29cf9d96717b30e4639d6
VishalSoni2005 <vsoni0882@gmail.com>
2025-03-18 23:08:14 +0530

    Debuging

--- Server/src/Routes/message.route.js
+++ Server/src/Routes/message.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getMessages, getUserForSidebar } from "../Controllers/message.controller.js";
+import { getMessages, getUserForSidebar, sendMessage } from "../Controllers/message.controller.js";
 import { authorizedRoute } from "../Middleware/authorise.middleware.js";
 const router = express.Router();
 
 router.get("/users", authorizedRoute ,getUserForSidebar);
 router.get("/:id", authorizedRoute ,getMessages)
+
+router.post('/send/:id', authorizedRoute, sendMessage)
 
 export default router;
